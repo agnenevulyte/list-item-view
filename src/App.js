1ec0efc6fda21d1ebe8fc87deaf9d652e971f06d
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-12 15:03:42 +0000

    added Router on top level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import List from "./components/List";
+import NotFound from "./NotFound";
+import ListItem from "./components/ListItem";
 
 function App() {
   return (
     <div className="App">
-      <List />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={List} />
+          <Route path="/:fistname" component={ListItem} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
