62a03fda3f2291fc9a1c44055bb698d7dfced0d8
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-12 15:04:38 +0000

    List has the state and fetches data

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,10 +1,56 @@
-import React from "react";
-import ListItem from "../ListItem";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { fetchData } from "./fetchData";
 
-export default function List() {
-  return (
-    <div>
-      <ListItem />
-    </div>
-  );
+export default class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: true,
+      error: null
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    try {
+      const res = await fetchData();
+      console.log("res:", res);
+      this.setState({
+        users: res.clients,
+        loading: false
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: error
+      });
+    }
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <Route
+          render={({ history }) => (
+            <ul>
+              {users &&
+                users.map(user => (
+                  <li
+                    key={user.id}
+                    onClick={() => history.push(`/${user.name}`)}
+                  >
+                    <h1>{user.name}</h1>
+                    <p>{user.description}</p>
+                  </li>
+                ))}
+            </ul>
+          )}
+        />
+      </div>
+    );
+  }
 }
