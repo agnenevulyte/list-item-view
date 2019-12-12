f96dc3258bb9ae3cabc42b7090b097920fa10587
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-12 11:49:23 +0000

    App.js imports List component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import List from "./components/List";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
 }
 
 export default App;
