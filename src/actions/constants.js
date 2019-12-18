12d647835b612e5d3f9e8632d5defb9037d06634
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-18 15:39:44 +0000

    created constants file for actions

---
+++ src/actions/constants.js
@@ -0,0 +1,10 @@
+// Redux also suggest defining constants for your action types.
+export const setActionTypes = {
+  USERS_LOADED: "USERS_LOADED",
+  USERS_LOADING: "USERS_LOADING",
+  USERS_LOADING_ERROR: "USERS_LOADING_ERROR",
+  USER_LOADED: "USER_LOADED",
+  USER_LOADING: "USER_LOADING",
+  USER_LOADING_ERROR: "USER_LOADING_ERROR"
+  // SELECT_USER: "SELECT_USER"
+};
