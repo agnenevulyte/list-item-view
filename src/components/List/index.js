43ae95a86976d7e33fc5ba22d5a62c1a1298eaa2
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-18 16:02:06 +0000

    List.js redux

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,56 +1,101 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import React, { useEffect, Component } from "react";
+import { Link, Route } from "react-router-dom";
 import { fetchData } from "./fetchData";
+import { connect } from "react-redux";
+import {
+  selectedUser,
+  usersLoaded,
+  usersLoading,
+  usersLoadingError
+} from "../../actions";
 
-export default class List extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      loading: true,
-      error: null
-    };
+class List extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   users: []
+    // };
   }
+  componentDidMount() {
+    this.props.usersLoading();
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    try {
-      const res = await fetchData();
-      console.log("res:", res);
-      this.setState({
-        users: res.clients,
-        loading: false
+    fetchData()
+      .then(res => {
+        console.log("res:", res.clients);
+        // this.setState({ users: res.clients });
+        this.props.usersLoaded(res.clients);
+      })
+      .catch(error => {
+        console.log(error);
+        this.props.usersLoadingError(error);
       });
-    } catch (error) {
-      console.log(error);
-      this.setState({
-        loading: false,
-        error: error
-      });
-    }
   }
 
   render() {
-    const { users } = this.state;
+    const { users } = this.props;
+    console.log("list users: ", users);
     return (
       <div>
-        <Route
-          render={({ history }) => (
-            <ul>
-              {users &&
-                users.map(user => (
-                  <li
-                    key={user.id}
-                    onClick={() => history.push(`/${user.name}`)}
-                  >
-                    <h1>{user.name}</h1>
-                    <p>{user.description}</p>
-                  </li>
-                ))}
-            </ul>
-          )}
-        />
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <Link to={`/${user.id}`}>
+                <h1>{user.name}</h1>
+                <p>{user.description}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  loading: state.users.loading,
+  users: state.users.users,
+  error: state.users.error
+});
+
+const mapDispatchToProps = {
+  usersLoaded,
+  usersLoading,
+  usersLoadingError
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
+
+// HOOCK
+// const List = ({ usersLoading, usersLoaded, usersLoadingError, users }) => {
+//   useEffect(() => {
+//     fetchUsers();
+//   });
+//   const fetchUsers = () => {
+//     usersLoading();
+//     fetchData()
+//       //   .then(results => console.log(results.clients))
+//       //   .then(res => usersLoaded(res.clients))
+//       .catch(error => {
+//         console.log(error);
+//         usersLoadingError(error);
+//       });
+//   };
+//   return (
+//     <div>
+//       <ul>
+//         {users.map(user => (
+//           <li key={user.id}>
+//             <Link to={`/${user.id}`}>
+//               <h1>{user.name}</h1>
+//               <p>{user.description}</p>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = ({ users }) => ({
+//   users
+// });
